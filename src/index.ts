6483a1d0646308b8e5b1388555b607ce04b6b10a
ccctao <106943088+ccctao@users.noreply.github.com>
2023-01-25 23:59:01 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
 import cron from 'node-cron';
+var PythonShell = require('python-shell');
 
-cron.schedule(`*/1 * * * *`, async () => {
+cron.schedule(`0 0 * * *`, async () => {
   console.log(`running your task...`);
+  await PythonShell.run('bot.py', options, function (err, results) {
+          //On 'results' we get list of strings of all print done in your py scripts sequentially. 
+          if (err) throw err;
+          console.log('results: ');
+          for(let i of results){
+                console.log(i, "---->", typeof i)
+          }
 });
